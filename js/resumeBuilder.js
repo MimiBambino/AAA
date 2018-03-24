6e9ed8656b48d89bdd3fbc6658683fb583e4e0b1
Cynthia O'Donnell <cynthiafussellodonnell@gmail.com>
2018-03-24 18:33:06 -0400

    lighten hover color

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,9 +2,7 @@
 
 $(document).ready(function () {
     $('div.hidden').fadeIn(1000).removeClass('hidden');
-    $('a.outbound').hover(function() {
-	$(this).animate({'background-color': "#5ec2d0"}, 'slow');
-});
+    
 });
 
 
